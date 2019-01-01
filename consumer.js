89f6bee0737f0d36f314d4ec26ca65cd6151aa30
Sparshith Sampath <sparshithsampath@gmail.com>
2019-01-01 14:15:48 +0530

    fixes

--- consumer.js
+++ consumer.js
@@ -13,14 +13,25 @@ const timeout = 60;
 // References an existing subscription
 const subscription = pubsub.subscription(subscriptionName);
 
+var gpio = require('rpi-gpio')
+var gpiop = gpio.promise;
+
+var gpio = require('rpi-gpio')
+var gpiop = gpio.promise;
+ 
+gpiop.setup(29, gpio.DIR_OUT)
 // Create an event handler to handle messages
 let messageCount = 0;
 const messageHandler = message => {
-  console.log(`Received message ${message.id}:`);
+  console.log(`Received message ${message.data.state}:`);
   console.log(`\tData: ${message.data}`);
   console.log(`\tAttributes: ${message.attributes}`);
   messageCount += 1;
-
+	if(messageCount % 2 === 0)  {
+		gpiop.write(29, true);	
+	} else {
+		gpiop.write(29, false);
+	}
   // "Ack" (acknowledge receipt of) the message
   message.ack();
 };
